2ba914b1b43608eaebcb036a3332193c9aac5b62
Maksym Rozhanskyi <maksym.rozhanskyi@itomy.ch>
2022-02-21 14:27:10 +0200

    add hash router for gh-page

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { store } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router >
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
